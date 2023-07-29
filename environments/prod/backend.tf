fd247983f1fdea5b7a4d5106c29cf889676cc101
amitg0098 <amit89.job@gmail.com>
2023-07-29 14:56:41 +0000

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "playground-s-11-e43f6bbc-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="playground-s-11-e43f6bbc"
